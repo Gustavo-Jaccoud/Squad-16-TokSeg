9bbd50b368eb5198ae190d200e809620357ebf82
Gustavo Jaccoud <gustavo.c.jaccoud@gmail.com>
2025-04-29 09:05:10 -0300

    feat: Create methods getAll and getById for apartment

--- src/main/java/com/tokseg/storage/controllers/apartment/ApartmentController.java
+++ src/main/java/com/tokseg/storage/controllers/apartment/ApartmentController.java
@@ -3,16 +3,15 @@ package com.tokseg.storage.controllers.apartment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.tokseg.storage.domain.apartment.DTOs.ApartmentDTO;
 import com.tokseg.storage.response.ApiResponse;
 import com.tokseg.storage.services.ApartmentService;
 
 import jakarta.validation.Valid;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("apartment")
@@ -28,5 +27,18 @@ public class ApartmentController {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
 
+    }
+    @GetMapping()
+    public ResponseEntity getAllApartment(){
+        return ResponseEntity.status(HttpStatus.OK).body(apartmentService.getAllApartment());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getByIdBlock(@PathVariable UUID id){
+        var response = apartmentService.getByIdApartment(id);
+        if (response.data() != null)
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 }

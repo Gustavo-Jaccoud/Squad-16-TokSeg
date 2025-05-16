2ce85575138effa1afce82213c42cb3de75df23b
Gustavo Jaccoud <gustavo.c.jaccoud@gmail.com>
2025-05-16 11:58:34 -0300

    chore: update Notification model

--- src/main/java/com/tokseg/storage/domain/notification/Notification.java
+++ src/main/java/com/tokseg/storage/domain/notification/Notification.java
@@ -1,10 +1,15 @@
 package com.tokseg.storage.domain.notification;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.tokseg.storage.domain.deliveryPackage.DeliveryPackage;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -19,13 +24,24 @@ public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private UUID deliveryPackageId;
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_package_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private DeliveryPackage deliveryPackage;
+
+    @JsonProperty("deliveryPackageId")
+    public UUID getBlockId() {
+        return deliveryPackage != null ? deliveryPackage.getId() : null;
+    }
+
+    @Column(nullable = false)
     private LocalDateTime sentDatetime;
-    @Column(name = "status")
+    @Column(name = "status",nullable = false)
     private NotificationStatus notificationStatus;
 
-    public Notification(UUID deliveryPackageId, NotificationStatus notificationStatus) {
-        this.deliveryPackageId = deliveryPackageId;
+    public Notification(DeliveryPackage deliveryPackage, NotificationStatus notificationStatus) {
+        this.deliveryPackage = deliveryPackage;
         this.notificationStatus = notificationStatus;
     }
 }
